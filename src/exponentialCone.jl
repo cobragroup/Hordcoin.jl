d54420723c85b6674a9340fe02b22ec6d94a6e0c
jakubkislinger <jakub.kislinger@dnai.ai>
2024-05-05 13:43:12 +0100

    Updating polymatroid to use NSB and work properly, nlp method added

--- src/exponentialCone.jl
+++ src/exponentialCone.jl
@@ -1,11 +1,11 @@
 # exponentialCone.jl:
 
-function cone_for_optimizer(joined_prob::Array{Float64}, marginals, optimizer::SCS.Optimizer)::EMResult
-    cone_over_probabilities(joined_prob, marginals; model = Model(typeof(optimizer)))
+function cone_for_optimiser(joined_prob::Array{Float64}, marginals, optimiser::SCS.Optimizer)::EMResult
+    cone_over_probabilities(joined_prob, marginals; model = Model(typeof(optimiser)))
 end
 
-function cone_for_optimizer(joined_prob::Array{Float64}, marginals, optimizer::MosekTools.Optimizer)::EMResult
-    cone_over_probabilities(joined_prob, marginals; model = Model(typeof(optimizer)))
+function cone_for_optimiser(joined_prob::Array{Float64}, marginals, optimiser::MosekTools.Optimizer)::EMResult
+    cone_over_probabilities(joined_prob, marginals; model = Model(typeof(optimiser)))
 end
 
 
@@ -38,4 +38,58 @@ function cone_over_probabilities(joined_prob::Array{Float64}, marginals; model::
 
     # this function uses natural logarithm, so it is need to take it in account
     return EMResult(objective_value(model) / log(2), value.(q))
+end
+
+function nlp_entropies_for_optimiser(joined_prob::Array{Float64}, marginal_size, optimiser::String)::EMResult
+    if (optimiser == "ipopt")
+        return nlp_fixed_entropies(joined_prob, marginal_size, model = Model(Ipopt.Optimizer))
+    elseif (optimiser == "madnlp")
+        return nlp_fixed_entropies(joined_prob, marginal_size, model = Model(MadNLP.Optimizer))
+    else
+        throw(ArgumentError("Invalid optimiser $optimiser. Choose between 'ipopt' and 'madnlp'"))
+    end
+end
+
+function nlp_fixed_entropies(joined_prob::Array{Float64}, marginal_size; model::Model = Model(Ipopt.Optimizer))::EMResult
+
+    num_dimensions = ndims(joined_prob)
+
+    # defines the complement of a set of dimension
+    ~(s) = (i for i = 1:num_dimensions if i ∉ s)
+
+    n = length(joined_prob)
+    
+    set_silent(model)
+
+    # define the result probabilities
+    @variable(model, p[1:n] >= 0)
+    @constraint(model, sum(p) == 1)
+
+    q = reshape(p, size(joined_prob)...)
+
+    for i in 1:marginal_size
+        entropies = permutations_of_length(i, num_dimensions)
+        for ent in eachcol(entropies)
+            d = Tuple(collect(~(ent)))
+
+            par_prob = dropdims(sum(joined_prob, dims = d), dims = d)
+            model_prob = dropdims(sum(q, dims = d), dims = d)
+
+            val = sum(par_prob[i] * log2(par_prob[i]) for i in 1:length(par_prob))
+
+            @NLconstraint(model, val == sum(model_prob[i] * log2(model_prob[i]) for i in 1:length(model_prob)))
+        end
+    end
+    
+    # Max ent. reformulation
+    @variable(model, t[1:n])
+
+    @NLconstraint(model, con[i in 1:n], t[i] == - p[i] * log2(p[i]))
+
+    @objective(model, Max, sum(t))
+
+    JuMP.optimize!(model)
+
+    # this function uses natural logarithm, so it is need to take it in account
+    return EMResult(objective_value(model) / log(2), value.(p))
 end
--- src/polymatroid.jl
+++ src/polymatroid.jl
@@ -1,74 +1,4 @@
-
-
-export polymatroid_optim
-export polymatroid_optim_ver2
-export polymatroid_optim_nsb
-
-# fixating entropy only for given size of marginal (not lower)
-function polymatroid_optim_ver2(joined_prob::Array{Float64}, marginal_size; model::Model = Model(Mosek.Optimizer))
-
-    set_silent(model)
-    
-    num_dimensions = ndims(joined_prob)
-
-    N = 1:num_dimensions
-
-    # doctionary set to index
-    s_i = Dict()
-
-    index = 1
-
-    # initialization of  non-negativity constraints
-    # 𝒉(𝐴) ≥ 0, ∀𝐴 ∈ 𝒫(𝑁)
-    # useless >= 0
-    @variable(model, h[1:(2^num_dimensions)] >= 0)  
-
-    for A in powerset(N)
-        s_i[A] = index
-        index += 1
-    end
-
-    # ∀𝐴 ⊆ 𝒫(𝑁)
-    for A in powerset(N)
-        if length(A) > num_dimensions - 2
-            continue
-        end
-
-        # submodularity 
-        # 𝒉(𝐴 ∪ 𝘪) + 𝒉(𝐴 ∪ 𝘫) ≥ 𝒉(𝐴 ∪ 𝘪𝘫) + 𝒉(𝐴)
-        for ij in powerset(setdiff(N, A), 2, 2)
-            i, j = ij
-            @constraint(model, h[s_i[sort(A ∪ i)]] + h[s_i[sort(A ∪ j)]] >= h[s_i[sort(A ∪ ij)]] + h[s_i[A]])
-        end
-    end
-
-    # monotonicity
-    # 𝒉(𝑁) ≥ 𝒉(𝑁 ∖ 𝘪), ∀𝑖 ∈ 𝑁
-    for i in N
-        @constraint(model, h[s_i[N]] >= h[s_i[setdiff(N, i)]])
-    end
-
-
-   
-    ~(s::Tuple) = (i for i = 1:ndims(joined_prob) if i ∉ s)
-    
-    marginals = permutations_of_length(marginal_size, ndims(joined_prob))
-
-    @constraint(model, [m in marginals], distribution_entropy(sum(joined_prob, dims = ~(m))) == h[s_i[collect(m)]])
-
-
-
-    # 𝘩(∅) = 0
-    @constraint(model, h[s_i[[]]] == 0)
-
-    @objective(model, Max, h[s_i[N]])
-
-    optimize!(model)
-
-    return objective_value(model), value.(h), s_i
-
-end
-
+# polymatroid.jl
 
 function polymatroid_optim(joined_prob::Array{Float64}, marginal_size; model::Model = Model(Mosek.Optimizer), zhang_yeung = false, mle_correction = 0)
 
@@ -117,9 +47,7 @@ function polymatroid_optim(joined_prob::Array{Float64}, marginal_size; model::Mo
     for i in 1:marginal_size
         marginals = permutations_of_length(i, num_dimensions)
         @constraint(model, [m in marginals], distribution_entropy(sum(joined_prob, dims = ~(m))) + mle_correction == h[s_i[collect(m)]])
-        # @show
     end
-
 
     # Zhang-Yeung
     if num_dimensions >= 4 && zhang_yeung
@@ -130,8 +58,6 @@ function polymatroid_optim(joined_prob::Array{Float64}, marginal_size; model::Mo
                 end
                 for kl in powerset(setdiff(N, [i, j]), 2, 2)
                     k, l = kl
-
-                    #@show i, j, k, l
                     
                     ij = s_i[sort(i ∪ j)]
                     ik = s_i[sort(i ∪ k)]
@@ -156,7 +82,6 @@ function polymatroid_optim(joined_prob::Array{Float64}, marginal_size; model::Mo
             end
         end
     end
-
 
 
     # 𝘩(∅) = 0
@@ -172,241 +97,13 @@ function polymatroid_optim(joined_prob::Array{Float64}, marginal_size; model::Mo
 
 end
 
-function polymatroid_optim_nsb(data, marginal_size; model::Model = Model(Mosek.Optimizer), zhang_yeung = false)
-
-    #n = size(data, 2)
-
-#    distribution = zeros(Int, [5 for i in 1:n]...);
-
-#    for x in eachrow(data);
-#        distribution[x...] += 1;
-#    end
-
-    distribution = data
-
-    set_silent(model)
-    
-    num_dimensions = ndims(distribution)
-
-    N = 1:num_dimensions
-
-    # doctionary set to index
-    s_i = Dict()
-
-    index = 1
-
-    # initialization of  non-negativity constraints
-    # 𝒉(𝐴) ≥ 0, ∀𝐴 ∈ 𝒫(𝑁)
-    @variable(model, h[1:(2^num_dimensions)] >= 0)  
-
-    for A in powerset(N)
-        s_i[A] = index
-        index += 1
-    end
-
-    # ∀𝐴 ⊆ 𝒫(𝑁)
-    for A in powerset(N)
-        if length(A) > num_dimensions - 2
-            continue
-        end
-
-        # submodularity 
-        # 𝒉(𝐴 ∪ 𝘪) + 𝒉(𝐴 ∪ 𝘫) ≥ 𝒉(𝐴 ∪ 𝘪𝘫) + 𝒉(𝐴)
-        for ij in powerset(setdiff(N, A), 2, 2)
-            i, j = ij
-            @constraint(model, h[s_i[sort(A ∪ i)]] + h[s_i[sort(A ∪ j)]] >= h[s_i[sort(A ∪ ij)]] + h[s_i[A]])
-        end
-    end
-
-    # monotonicity
-    # 𝒉(𝑁) ≥ 𝒉(𝑁 ∖ 𝘪), ∀𝑖 ∈ 𝑁
-    for i in N
-        @constraint(model, h[s_i[N]] >= h[s_i[setdiff(N, i)]])
-    end
-   
-    ~(s::Tuple) = (i for i = 1:num_dimensions if i ∉ s)
-
-    for i in 1:marginal_size
-        marginals = permutations_of_length(i, num_dimensions)
-        @constraint(model, [m in marginals], call_nsb_octave(sum(distribution, dims = ~(m))) == h[s_i[collect(m)]])
-        # @show
-    end
-
-
-    # Zhang-Yeung
-    if num_dimensions >= 4 && zhang_yeung
-        for i in N
-            for j in N
-                if i == j
-                    continue
-                end
-                for kl in powerset(setdiff(N, [i, j]), 2, 2)
-                    k, l = kl
-
-                    @show i, j, k, l
-                    
-                    ij = s_i[sort(i ∪ j)]
-                    ik = s_i[sort(i ∪ k)]
-                    il = s_i[sort(i ∪ l)]
-                    jk = s_i[sort(j ∪ k)]
-                    jl = s_i[sort(j ∪ l)]
-                    kl = s_i[sort(k ∪ l)]
-                    ikl = s_i[sort(i ∪ k ∪ l)]
-                    jkl = s_i[sort(j ∪ k ∪ l)]
-
-                    i_i = s_i[[i]]
-                    i_k = s_i[[k]]
-                    i_l = s_i[[l]]
-
-                    @constraint(
-                        model, 
-                        3*(h[ik] + h[il] + h[kl]) 
-                        + h[jk] + h[jl] - h[i_i] 
-                        - 2*(h[i_k] + h[i_l]) - h[ij] 
-                        - 4*h[ikl] - h[jkl] ≥ 0)
-                end
-            end
-        end
-    end
-
-    # 𝘩(∅) = 0
-    @constraint(model, h[s_i[[]]] == 0)
-
-    @objective(model, Max, h[s_i[N]])
-
-    #@show model
-
-    optimize!(model)
-
-    return objective_value(model), value.(h), s_i
-
-end
-
-function polymatroid_optim_general(data::Array{Int}, marginal_size; model::Model = Model(Mosek.Optimizer), zhang_yeung = false, mle_correction = 0, ent_method = "categoric")
-
-    set_silent(model)
-    
-    num_dimensions = ndims(data)
-
-    if ent_method == "categoric"
-        joined_prob = data ./ sum(data)
-    end
-
-    N = 1:num_dimensions
-
-    # doctionary set to index
-    s_i = Dict()
-
-    index = 1
-
-    # initialization of  non-negativity constraints
-    # 𝒉(𝐴) ≥ 0, ∀𝐴 ∈ 𝒫(𝑁)
-    @variable(model, h[1:(2^num_dimensions)] >= 0)  
-
-    ent = zeros(2^num_dimensions)
-
-    for A in powerset(N)
-        s_i[A] = index
-        index += 1
-    end
-
-    # ∀𝐴 ⊆ 𝒫(𝑁)
-    for A in powerset(N)
-        if length(A) > num_dimensions - 2
-            continue
-        end
-
-        # submodularity 
-        # 𝒉(𝐴 ∪ 𝘪) + 𝒉(𝐴 ∪ 𝘫) ≥ 𝒉(𝐴 ∪ 𝘪𝘫) + 𝒉(𝐴)
-        for ij in powerset(setdiff(N, A), 2, 2)
-            i, j = ij
-            @constraint(model, h[s_i[sort(A ∪ i)]] + h[s_i[sort(A ∪ j)]] >= h[s_i[sort(A ∪ ij)]] + h[s_i[A]])
-        end
-    end
-
-    # monotonicity
-    # 𝒉(𝑁) ≥ 𝒉(𝑁 ∖ 𝘪), ∀𝑖 ∈ 𝑁
-    for i in N
-        @constraint(model, h[s_i[N]] >= h[s_i[setdiff(N, i)]])
-    end
-   
-    ~(s::Tuple) = (i for i = 1:ndims(data) if i ∉ s)
-
-    for i in 1:marginal_size
-        marginals = permutations_of_length(i, num_dimensions)
-        for m in marginals
-            if ent[s_i[collect(m)]] == 0
-                if ent_method == "categoric"
-                    ent[s_i[collect(m)]] = distribution_entropy(sum(joined_prob, dims = ~(m)))
-                elseif ent_method == "nsb"
-                    ent[s_i[collect(m)]] = call_nsb_octave(sum(data, dims = ~(m)))
-                else
-                    throw(DomainError("Unknown entropy method"))
-                end
-            end
-        end
-        @constraint(model, [m in marginals], ent[s_i[collect(m)]] + mle_correction == h[s_i[collect(m)]])
-        # @show
-    end
-
-
-    # Zhang-Yeung
-    if num_dimensions >= 4 && zhang_yeung
-        for i in N
-            for j in N
-                if i == j
-                    continue
-                end
-                for kl in powerset(setdiff(N, [i, j]), 2, 2)
-                    k, l = kl
-
-                    #@show i, j, k, l
-                    
-                    ij = s_i[sort(i ∪ j)]
-                    ik = s_i[sort(i ∪ k)]
-                    il = s_i[sort(i ∪ l)]
-                    jk = s_i[sort(j ∪ k)]
-                    jl = s_i[sort(j ∪ l)]
-                    kl = s_i[sort(k ∪ l)]
-                    ikl = s_i[sort(i ∪ k ∪ l)]
-                    jkl = s_i[sort(j ∪ k ∪ l)]
-
-                    i_i = s_i[[i]]
-                    i_k = s_i[[k]]
-                    i_l = s_i[[l]]
-
-                    @constraint(
-                        model, 
-                        3*(h[ik] + h[il] + h[kl]) 
-                        + h[jk] + h[jl] - h[i_i] 
-                        - 2*(h[i_k] + h[i_l]) - h[ij] 
-                        - 4*h[ikl] - h[jkl] ≥ 0)
-                end
-            end
-        end
-    end
-
-    # 𝘩(∅) = 0
-    @constraint(model, h[s_i[[]]] == 0)
-
-    @objective(model, Max, h[s_i[N]])
-
-    optimize!(model)
-
-    #@show ent
-    #@show h
-
-    return objective_value(model), value.(h), s_i
-
-end
-
 function polymatroid_most_gen(method::PolymatroidEntropyMethod,
                               data::Array{Int}, 
                               marginal_size::Int;
-                              zhang_yeung = false, 
-                              model::Model = Model(Mosek.Optimizer),
-                              precalculated_entropies = nothing,
+                              precalculated_entropies = Dict(),
                               set_to_index = Dict())
+
+    model = Model(typeof(method.optimiser))
     set_silent(model)
     
     num_dimensions = ndims(data)
@@ -415,14 +112,7 @@ function polymatroid_most_gen(method::PolymatroidEntropyMethod,
 
     # doctionary set to index
     s_i = set_to_index
-    if precalculated_entropies !== nothing
-        if (length(precalculated_entropies) != 2^num_dimensions)
-            throw(DomainError("Precalculated entropies have to be of size 2^num_dimensions"))
-        end
-        ent = precalculated_entropies
-    else
-        ent = zeros(2^num_dimensions)
-    end
+    ent = precalculated_entropies
 
     index = maximum(values(s_i), init = 0) + 1
 
@@ -464,20 +154,26 @@ function polymatroid_most_gen(method::PolymatroidEntropyMethod,
     for i in 1:marginal_size
         marginals = permutations_of_length(i, num_dimensions)
         for m in marginals
-            if ent[s_i[collect(m)]] == 0
-                ent[s_i[collect(m)]] = entropy(data, method, ~(m))
+            if !haskey(ent, collect(m))
+                ent[collect(m)] = entropy(data, method, ~(m))
             end
         end
         ent_con[i] = Array{Any, 1}(undef, length(marginals))
         for j in 1:length(marginals)
             m = marginals[j]
-            ent_con[i][j] = @constraint(model, h[s_i[collect(m)]] == ent[s_i[collect(m)]])
+            #ent_con[i][j] = @constraint(model, h[s_i[collect(m)]] == ent[s_i[collect(m)]])
+            if (method isa NsbPolymatroid && method.tolerance > 0)
+                @constraint(model, h[s_i[collect(m)]] >= (1 - method.tolerance) * ent[collect(m)])
+                @constraint(model, h[s_i[collect(m)]] <= (1 + method.tolerance) * ent[collect(m)])
+            else
+                @constraint(model, h[s_i[collect(m)]] == ent[collect(m)])
+            end
         end
     end
 
 
     # Zhang-Yeung
-    if num_dimensions >= 4 && zhang_yeung
+    if num_dimensions >= 4 && method.zhang_yeung
         for i in N
             for j in N
                 if i == j
@@ -485,8 +181,6 @@ function polymatroid_most_gen(method::PolymatroidEntropyMethod,
                 end
                 for kl in powerset(setdiff(N, [i, j]), 2, 2)
                     k, l = kl
-
-                    #@show i, j, k, l
                     
                     ij = s_i[sort(i ∪ j)]
                     ik = s_i[sort(i ∪ k)]
@@ -519,9 +213,10 @@ function polymatroid_most_gen(method::PolymatroidEntropyMethod,
 
     optimize!(model)
 
-    if (!is_solved_and_feasible(model))
-        throw(DomainError("Model is not feasible with method $(method) and marginal size $(marginal_size)"))
-    end
+    # TODO: JuMP bug - method not found, but should exist
+    #if (!is_solved_and_feasible(model))
+    #    throw(DomainError("Model is not feasible with method $(method) and marginal size $(marginal_size)"))
+    #end
 
     return objective_value(model), value.(h), ent, s_i
 
@@ -533,4 +228,28 @@ end
 
 function entropy(data::Array{Int}, method::NsbPolymatroid, inverse_marginals)
     return call_nsb_octave(sum(data, dims = inverse_marginals))
+end
+
+export precompute_entropies
+
+function precompute_entropies(data::Array{Int})
+
+    entropies = Dict()
+
+    num_dimensions = ndims(data)
+
+    @show num_dimensions
+
+    ~(s::Tuple) = (i for i = 1:ndims(data) if i ∉ s)
+
+    for i in 1:num_dimensions
+        @show i
+        marginals = permutations_of_length(i, num_dimensions)
+        for m in marginals
+            entropies[collect(m)] = entropy(data, NsbPolymatroid(), ~(m))
+        end 
+    end
+
+    return entropies
+
 end
